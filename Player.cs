d1b1121ef8cf42d7e34b05cd548d054fd27fff21
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-01-10 12:37:36 +0530

    Player

---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿class Player
+{
+    public int PlayerId { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Player(int playerId, string name, int age)
+    {
+        PlayerId = playerId;
+        Name = name;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"Player ID: {PlayerId}, Name: {Name}, Age: {Age}";
+    }
+}
